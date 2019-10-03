599c39c3f0409c8e997639c725239d1a8f8dffd8
bruno.jesus <bruno.jesus@marimex.com.br>
2019-10-03 16:46:38 -0300

    first commit

---
+++ Transation_RollBack/Context/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transation_RollBack.Models;
+
+
+namespace Transation_RollBack.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext():base("BancoContexto")
+        {
+        }
+
+        public DbSet<People> People { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+        }
+    }
+}
---
+++ Transation_RollBack/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transation_RollBack.Context;
+using Transation_RollBack.Models;
+
+namespace Transation_RollBack
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            using(var ctx = new DataContext())
+            {
+                using (var transation = ctx.Database.BeginTransaction())
+                {
+                    
+                    ctx.People.Add(new People { Name = "Carlos" });
+                    ctx.SaveChanges();
+
+                    ctx.Employee.Add(new Employee { Name = "Develop Systems 1" });
+                    ctx.SaveChanges();
+
+                    if (ctx.People.ToList().Count() > 0) 
+                    {
+                        transation.Rollback();
+                        throw new Exception("Transação cancelada!");
+                    }
+
+                    transation.Commit();                    
+                }
+            }
+        }
+    }
+}
